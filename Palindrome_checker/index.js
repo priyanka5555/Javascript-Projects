f1fd7a8f509ae0b7e1872e9ca2a835c8f221898e
Priyanka <priyankasharma080396@gmail.com>
2025-01-09 17:38:07 -0500

    added palindrome logic for word

--- Palindrome_checker/index.js
+++ Palindrome_checker/index.js
@@ -1,25 +1,42 @@
-const input_name = document.getElementById('name1')
-const output = document.getElementById('output')
 
-function getValue() {
-    let original_number = parseInt(input_name.value)
-    let  number = original_number
-    let  reversed_number = 0
-    let rem = 0
+input_number = document.getElementById("name1")
+// output = document.getElementById("output")
 
-    while (original_number > 0) {
-        rem = original_number % 10
-        reversed_number = reversed_number * 10 + rem
-        original_number = Math.floor(original_number/10)
+function isPalndrome() {
+    let regex = /^[a-zA-Z0-9]+$/;
+
+
+    let original_value = input_number.value
+    let x = original_value
+    let reversed_number = 0
+    let reminder = 0
+
+    if (!original_value.match(regex)) {
+        while (x != 0) {
+
+            reminder = x % 10
+            reversed_number = reversed_number * 10 + reminder
+            x = Math.floor(x / 10)
+        }
+
+        if (original_value == reversed_number) {
+            output.innerHTML = original_value + " is a Palindrome"
+        }
+        else {
+            output.innerHTML = original_value + " is not a Palindrome"
+        }
+    }
+    else {
+        const reversed_word = original_value.split("").reverse().join("");
+
+        if(reversed_word == original_value){
+            output.innerHTML = original_value + " is a Palindrome"
+        }
+
+        else{
+            output.innerHTML = original_value + " is not a Palindrome"
+        }
+        
     }
 
-    if(number == reversed_number){
-        output.innerHTML =  reversed_number + " is a palindrome"
     }
-    else{
-        output.innerHTML =  reversed_number + " is not a palindrome"
-    }
-
-    
-
-}
