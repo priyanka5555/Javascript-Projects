9d8788d45a6be8d64d6e9377af5b4a8959514254
Priyanka <priyankasharma080396@gmail.com>
2025-01-09 21:30:52 -0500

    added error handling logic

--- Palindrome_checker/index.js
+++ Palindrome_checker/index.js
@@ -1,42 +1,71 @@
 
-input_number = document.getElementById("name1")
-// output = document.getElementById("output")
+const input_number = document.getElementById("name1")
+const error_msg = document.getElementById("error_msg")
+const output = document.getElementById("output")
 
 function isPalndrome() {
-    let regex = /^[a-zA-Z0-9]+$/;
+    try {
+        let regex = /^[a-zA-Z]+$/;
 
 
-    let original_value = input_number.value
-    let x = original_value
-    let reversed_number = 0
-    let reminder = 0
+        let original_value = input_number.value.trim()
+        let x = original_value
+        let reversed_number = 0
+        let reminder = 0
 
-    if (!original_value.match(regex)) {
-        while (x != 0) {
 
-            reminder = x % 10
-            reversed_number = reversed_number * 10 + reminder
-            x = Math.floor(x / 10)
+        if (/[a-zA-Z]/.test(original_value) && /\d/.test(original_value) || /[^a-zA-Z0-9]/.test(original_value)) {
+
+            throw new Error("Alphanumberic and special characters are not allowed!");
+            // error_msg.innerHTML = "Alphanumberic and special characters are not allowed!"
+
         }
+        if (!original_value.match(regex)) {
 
-        if (original_value == reversed_number) {
-            output.innerHTML = original_value + " is a Palindrome"
+            while (x != 0) {
+
+                reminder = x % 10
+                reversed_number = reversed_number * 10 + reminder
+                x = Math.floor(x / 10)
+            }
+
+            if (original_value == reversed_number) {
+
+                output.innerHTML = original_value + " is a Palindrome"
+                error_msg.style.display = 'none'
+                output.style.display = 'block'
+            }
+            else {
+                output.innerHTML = original_value + " is not a Palindrome"
+                error_msg.style.display = 'none'
+                output.style.display = 'block'
+            }
         }
         else {
-            output.innerHTML = original_value + " is not a Palindrome"
-        }
-    }
-    else {
-        const reversed_word = original_value.split("").reverse().join("");
 
-        if(reversed_word == original_value){
-            output.innerHTML = original_value + " is a Palindrome"
+            const reversed_word = original_value.split("").reverse().join("");
+
+            if (reversed_word.toLowerCase() == original_value.toLowerCase()) {
+                output.innerHTML = original_value + " is a Palindrome"
+                error_msg.style.display = 'none'
+                output.style.display = 'block'
+            }
+
+            else {
+                output.innerHTML = original_value + " is not a Palindrome"
+                error_msg.style.display = 'none'
+                output.style.display = 'block'
+            }
+
         }
 
-        else{
-            output.innerHTML = original_value + " is not a Palindrome"
-        }
-        
-    }
 
     }
+    catch (err) {
+        // console.error("Error caught:", err);
+        error_msg.innerHTML = "Error: " + err.message;
+        output.style.display = 'none'
+        error_msg.style.display = 'block'
+    }
+
+}
